c731fe8b13f5dd68d8436c1f4dff08b761dbc675
perkasalintang52 <98840516+perkasalintang52@users.noreply.github.com>
2022-07-02 14:06:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -41,26 +41,26 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['6285822347348'] //ur owner number
-global.ownername = "ArullOfc"
-global.ytname = "YT:ARULL OFC " //ur yt chanel name
-global.socialm = "GitHub: Warikrr" //ur github or insta name
-global.location = "Indonesia, Kalimantan Selatan, Kotabaru" //ur location
+global.owner = ['6281322577123'] //ur owner number
+global.ownername = "LINTANG"
+global.ytname = "YT:Lintang Perkasa " //ur yt chanel name
+global.socialm = "GitHub: Perkasalintang52" //ur github or insta name
+global.location = "Indonesia, DKI JAKARTA, Pondok Indah" //ur location
 
 //bot bombdy
-global.botname = "ArullBotz" //ur bot name
-global.websitex = "https://youtube.com/channel/UC1ikkqsPvtWuE_QonsET1yg" //ur website
+global.botname = "LINTANG BOT" //ur bot name
+global.websitex = "https://tiktok.com/@joceylintangg" //ur website
 global.vidmenu = { url: 'https://a.uguu.se/eFNTvNt.mp4' } //gif and video menu
 global.packname = "Sticker By"
-global.author = "ArullBotz"
+global.author = "LINTANG BOT"
 global.themeemoji = "🐶"
 global.reactmoji = "🗿"
-global.ownertag = ['ArullOfc'] //ur owner tag
-global.ownernummenu = ['6285822347348'] //ur owner number in menu and all
-global.watermark = "ArullBotz" //ur watermark
-global.botscript = "https://youtu.be/Gr6yuCbSGTI"
-global.linkz1 = "https://chat.whatsapp.com/F6CAAOhrORm1sPdq6cRKAS"
-global.linkz2 = "https://youtube.com/channel/UC1ikkqsPvtWuE_QonsET1yg"
+global.ownertag = ['LINTANG'] //ur owner tag
+global.ownernummenu = ['6281322577123'] //ur owner number in menu and all
+global.watermark = "LINTANG BOT" //ur watermark
+global.botscript = "https://tiktok.com/@joceylintangg"
+global.linkz1 = "https://tiktok.com/@joceylintangg"
+global.linkz2 = "https://tiktok.com/@joceylintangg"
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -92,7 +92,7 @@ global.ntilink = []
 global.lolhuman = "KaysaS"
 global.sessionName = "session"
 global.antitags = true
-global.prefa = ['','!','.','🐦','🐤','🗿']
+global.prefa = ['']
 global.mess = {
     success: 'Done✓',
     admin: '*Khusus Admin*',
